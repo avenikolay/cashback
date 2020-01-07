eaaa846432b5224f67b6635bc83802a8e02ea414
Nikolay Kozlov <nikolay@velkomb.com>
2020-01-07 11:06:13 +0500

    Increased and Speacial cashback added

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,15 @@
-const regularPurchases = 300000;
+const regularPurchases = 100;
 const regularPurchasesPercent = 0.01;
-let cashback = regularPurchases * regularPurchasesPercent;
+
+const increasedPurchases = 100;
+const increasedPurchasesPercent = 0.05;
+
+const specialPurchases = 100;
+const specialPurchasesPercent = 0.3;
+
+let cashback = regularPurchases * regularPurchasesPercent +
+    increasedPurchases * increasedPurchasesPercent +
+    specialPurchases * specialPurchasesPercent;
 
 const cashbackLimit = 3000;
 if (cashback > cashbackLimit) {
